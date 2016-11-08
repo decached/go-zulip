8a91f6eb01f8a8343a041bd8bfad9cd49c153781
Akash Kothawale <akash@decached.com>
2016-11-08 11:28:09 -0500

    test: NewClient()

---
+++ zulip/zulip_test.go
@@ -0,0 +1,36 @@
+// zulip_test.go
+// Copyright (C) 2016 akash <akash@decached.com>
+//
+// Distributed under terms of the MIT license.
+
+package zulip
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+var (
+	mux    *http.ServeMux
+	client *Client
+	server *httptest.Server
+)
+
+func setup() {
+	mux = http.NewServeMux()
+	server = httptest.NewServer(mux)
+	client = NewClient(nil)
+
+	url, _ := url.Parse(server.URL)
+	client.BaseURL = url
+}
+
+func TestNewClient(t *testing.T) {
+	c := NewClient(nil)
+
+	if got, want := c.BaseURL.String(), defaultBaseURL; got != want {
+		t.Errorf("NewClient BaseURL is %v, want %v", got, want)
+	}
+}
